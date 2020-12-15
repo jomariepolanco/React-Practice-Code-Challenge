c20deeba3be1ccba5962e663a9b7dd928edfadc6
jomariepolanco <jomariepolanco@gmail.com>
2020-12-15 15:15:58 -0500

    get fetch done

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -7,6 +7,14 @@ const API = "http://localhost:3000/sushis"
 
 class App extends Component {
 
+  state = {
+    sushis: []
+  }
+
+  componentDidMount(){
+    fetch(API).then(r => r.json()).then(sushis => this.setState({sushis}))
+  }
+
   render() {
     return (
       <div className="app">
